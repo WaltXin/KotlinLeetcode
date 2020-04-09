67f3494032f1435cc5d0a50d702a814db9aaaffa
WaltXin <xiongx@tcd,ie>
2020-04-09 16:56:31 -0700

    Deligation

--- src/BestPractices/BestPractice.kt
+++ src/BestPractices/BestPractice.kt
@@ -1,5 +1,8 @@
 package BestPractices
 
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
 var _os: String? = null
 var os: String
     get() {
@@ -15,5 +18,32 @@ var os: String
 
 //convert to lazy implement
 val os1: String by lazy {
-    "create an new os"
+    println("create an new os")
+    "new os"
+}
+
+//property delegation
+class ownProperty(var field: String) {
+    operator fun getValue(thisRef: Any?, p: KProperty<*>) : String {
+        println("you read me")
+        return field
+    }
+
+    operator fun setValue(thisRef: Any?, p: KProperty<*>, value: String) {
+        println("you write me")
+        field = value
+    }
+}
+
+var observeMe by Delegates.observable("a") {
+    p, old, new ->
+    println("${p.name} goes $old to $new")
+}
+
+fun main() {
+    println(os1)
+    var p1 by ownProperty("p1 by own property")
+    println(p1)
+    observeMe = "bb"
+    observeMe = "cc"
 }
