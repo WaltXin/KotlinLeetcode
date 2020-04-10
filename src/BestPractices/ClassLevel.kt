449a685fc20f186ab6d98b15f7801736855d973e
WaltXin <xiongx@tcd.ie>
2020-04-10 10:30:45 -0700

    use companion object to create a factory.

--- src/BestPractices/ClassLevel.kt
+++ src/BestPractices/ClassLevel.kt
@@ -1,5 +1,6 @@
 package BestPractices
 
+import jdk.nashorn.internal.runtime.regexp.JoniRegExp
 import java.util.Comparator
 
 //Object key word
@@ -34,10 +35,33 @@ class Person(val name: String, val age: Int) {
             return o1.name.compareTo(o2.name)
         }
     }
+
+    //similar with java static
+    //companion object to create a factory
+    companion object {
+        //factory method, Create person by gender
+        fun createPerson(gender: Gender) {
+            when (gender) {
+                Gender.MALE -> Person("male", 24)
+                Gender.FEMALE -> Person("female", 24)
+            }
+        }
+    }
+}
+
+enum class Gender {
+    MALE,
+    FEMALE
 }
 
 fun main() {
     var course = Course("math")
     Courses.init(course)
     println(findCourse())
+    var p1 = Person("xin", 28)
+    var p2 = Person("xin", 29)
+    var samePerson = Person.personComparator.compare(p1, p2) >= 0
+    println(samePerson)
+    Person.createPerson("xin", 30)
+
 }
