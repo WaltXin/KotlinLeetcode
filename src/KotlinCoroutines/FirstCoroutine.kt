e876dc07932bf56d29eaa896656ca49d58e0eec9
WaltXin <xiongx@tcd.ie>
2020-04-13 15:08:08 -0700

    coroutine.

---
+++ src/KotlinCoroutines/FirstCoroutine.kt
@@ -0,0 +1,26 @@
+package KotlinCoroutines
+
+import kotlinx.coroutines.GlobalScope
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
+import kotlinx.coroutines.launch
+
+
+fun main() {
+    var result = AtomicInteger()
+
+    for (i in 1..15_000) {
+        thread(start = true) {
+            result.getAndIncrement()
+        }
+    }
+
+    for (i in 1..1_500_000) {
+        GlobalScope.launch {
+            result.getAndIncrement()
+        }
+    }
+
+    Thread.sleep(1000L)
+    println(result.get())
+}
